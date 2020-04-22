a0a5853e60667e1e3da6bcf8b59c09fcce793daa
ht-chchoi <chchoi@hdtel.co.kr>
2020-04-23 08:37:32 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/NettyexApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyexApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(NettyexApplication.class, args);
+    }
+
+}
